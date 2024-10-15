527f174dc826be63444875f3c85606900cb681cb
Administrator <Administrator@SYSTEMA33.WINSR2.COM>
2024-10-15 10:19:13 +0530

    morning

--- app/src/main/java/notes/NotesActivity.java
+++ app/src/main/java/notes/NotesActivity.java
@@ -19,14 +19,14 @@ public class NotesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_notes);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
         if(savedInstanceState == null){
-            NotesDisplayFragment notesDisplayFragmentFragement = new NotesDisplayFragment();
-            getSupportFragmentManager().beginTransaction().replace(R.id.main, notesDisplayFragmentFragement).commit();
+            NotesDisplayFragment notesDisplayFragment = new NotesDisplayFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.main, notesDisplayFragment).commit();
         }
 
         getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback( true) {
